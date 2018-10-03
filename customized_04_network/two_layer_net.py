29d3f2cb88e2a5a2280eb80f8d7489c8d92c350d
YutaroOhno <utr066@yahoo.co.jp>
2018-10-03 22:42:08 +0900

    使わない関数を削除

--- customized_04_network/two_layer_net.py
+++ customized_04_network/two_layer_net.py
@@ -1,9 +1,8 @@
 # coding: utf-8
 import sys, os
 sys.path.append(os.pardir)  # 親ディレクトリのファイルをインポートするための設定
-from common.functions import *
+from functions import *
 from common.gradient import numerical_gradient
-
 
 class TwoLayerNet:
 
