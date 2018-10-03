77dee5eb5a4e09eb8c2bf981c0a0553a75efd1d6
YutaroOhno <utr066@yahoo.co.jp>
2018-10-03 23:01:20 +0900

    無駄なものを削除

---
+++ customized_backpropagation_mnist(ch04)/train_neuralnet.py
@@ -0,0 +1,38 @@
+# coding: utf-8
+import sys, os
+sys.path.append(os.pardir)  # 親ディレクトリのファイルをインポートするための設定
+import numpy as np
+import matplotlib.pyplot as plt
+from dataset.mnist import load_mnist
+from two_layer_net import TwoLayerNet
+
+# データの読み込み
+(x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, one_hot_label=True)
+
+network = TwoLayerNet(input_size=784, hidden_size=50, output_size=10)
+
+iters_num = 10000  # 繰り返しの回数を適宜設定する
+train_size = x_train.shape[0] #60000
+batch_size = 100
+learning_rate = 0.1
+
+iter_per_epoch = max(train_size / batch_size, 1)
+
+for i in range(iters_num):
+    # ランダムにデータを選択
+    batch_mask = np.random.choice(train_size, batch_size)
+    x_batch = x_train[batch_mask]
+    t_batch = t_train[batch_mask]
+
+    # 逆伝播により勾配を計算
+    grad = network.gradient(x_batch, t_batch)
+
+    # パラメータの更新
+    for key in ('W1', 'b1', 'W2', 'b2'):
+        network.params[key] -= learning_rate * grad[key]
+
+    # モデルの正答率を取得
+    if i % iter_per_epoch == 0:
+        train_acc = network.accuracy(x_train, t_train)
+        test_acc = network.accuracy(x_test, t_test)
+        print("train acc, test acc | " + str(train_acc) + ", " + str(test_acc))
